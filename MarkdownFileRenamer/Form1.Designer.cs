41bdb4c6f3560c42af2d314babd171b3197b6351
sametcn99 <sametcn99@gmail.com>
2023-11-02 06:15:04 +0300

    Add project files.

---
+++ MarkdownFileRenamer/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MarkdownFileRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.selectFolderButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.selectedFolderLabel = new System.Windows.Forms.Label();
+            this.runButton = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // selectFolderButton
+            // 
+            this.selectFolderButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.selectFolderButton.Location = new System.Drawing.Point(12, 106);
+            this.selectFolderButton.Name = "selectFolderButton";
+            this.selectFolderButton.Size = new System.Drawing.Size(576, 75);
+            this.selectFolderButton.TabIndex = 0;
+            this.selectFolderButton.Text = "Select Folder";
+            this.selectFolderButton.UseVisualStyleBackColor = true;
+            this.selectFolderButton.Click += new System.EventHandler(this.selectFolderButton_Click);
+            // 
+            // selectedFolderLabel
+            // 
+            this.selectedFolderLabel.AutoSize = true;
+            this.selectedFolderLabel.Enabled = false;
+            this.selectedFolderLabel.ForeColor = System.Drawing.Color.White;
+            this.selectedFolderLabel.Location = new System.Drawing.Point(12, 9);
+            this.selectedFolderLabel.Name = "selectedFolderLabel";
+            this.selectedFolderLabel.Size = new System.Drawing.Size(39, 13);
+            this.selectedFolderLabel.TabIndex = 1;
+            this.selectedFolderLabel.Text = "Output";
+            // 
+            // runButton
+            // 
+            this.runButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.runButton.Enabled = false;
+            this.runButton.Location = new System.Drawing.Point(15, 187);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(576, 75);
+            this.runButton.TabIndex = 2;
+            this.runButton.Text = "RUN";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Location = new System.Drawing.Point(194, 266);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(196, 13);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Check out the Source Code from Github";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.White;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(603, 288);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.selectedFolderLabel);
+            this.Controls.Add(this.selectFolderButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Markdown File Renamer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button selectFolderButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label selectedFolderLabel;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ MarkdownFileRenamer/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace MarkdownFileRenamer
+{
+    public partial class Form1 : Form
+    {
+        string selectedFolderPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Method called when the "Select Folder" button is clicked
+        private void selectFolderButton_Click(object sender, EventArgs e)
+        {
+            // Create a folder browser dialog for selecting a directory
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Select a folder";
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                // Store the selected folder path
+                selectedFolderPath = fbd.SelectedPath;// Store the selected folder path
+                selectedFolderLabel.Enabled = true;// Enable the "Selected Folder" label
+                selectedFolderLabel.Text = selectedFolderPath;// Display the selected folder path
+                runButton.Enabled = true;// Enable the "Run" button
+            }
+            else
+            {
+                Console.WriteLine("Folder not found.");
+            }
+        }
+
+        // Method to rename and move files
+        private void RenameFiles()
+        {
+
+            // Get the parent folder of the selected folder
+            string parentFolder = Path.GetDirectoryName(selectedFolderPath);
+            // Create a new folder for the renamed files
+            string newFolderPath = Path.Combine(parentFolder, "RenamedFiles");
+
+            // Check if the new folder doesn't exist, and if not, create it
+            if (!Directory.Exists(newFolderPath))
+            {
+                Directory.CreateDirectory(newFolderPath);// Create the new folder
+            }
+
+            // Check if the selected folder exists
+            if (Directory.Exists(selectedFolderPath))// Check if the selected folder exists
+            {
+                // Get a list of Markdown files in the selected folder
+                string[] markdownFiles = Directory.GetFiles(selectedFolderPath, "*.md");// Get all files with the ".md" extension
+                foreach (var markdownFile in markdownFiles)// Loop through the Markdown files
+                {
+                    // Read the content of the Markdown file
+                    string markdownContent = File.ReadAllText(markdownFile);
+                    // Get a new file name based on the Markdown content
+                    string newFileName = GetTitle(markdownContent);// Call the GetTitle method
+
+                    if (!string.IsNullOrEmpty(newFileName))
+                    {
+                        // Create a new file path for the renamed file
+                        string newFilePath = Path.Combine(newFolderPath, newFileName + ".md");// Add the ".md" extension
+                        // Copy the original file to the new location
+                        try
+                        {
+                            File.Copy(markdownFile, newFilePath);// Copy the file
+                        }
+                        catch (Exception)// If the file already exists, show a message box
+                        {
+                            MessageBox.Show("Files already exist");
+                            break;
+                        }
+                    }
+                }
+                // Open the new folder with Windows Explorer
+                Process.Start("explorer.exe", newFolderPath);
+            }
+        }
+
+        // Method used to extract the title from the content of a Markdown file
+        static string GetTitle(string content)
+        {
+            // Split the content into lines
+            string[] lines = content.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);// Split the content into lines
+
+            if (lines.Length >= 2)// Check if the content has at least 2 lines
+            {
+                // Extract the second line as the title line
+                string titleLine = lines[1];
+                if (titleLine.Length >= 10)// Check if the title line is long enough
+                {
+                    // Remove unnecessary characters from the title
+                    titleLine = titleLine.Remove(0, 8);// Remove the first 8 characters
+                    titleLine = titleLine.Remove(titleLine.Length - 1);// Remove the last character
+                    // Sanitize the title by removing special characters
+                    string sanitizedString = SanitizeString(titleLine);// Call the SanitizeString method
+                    return sanitizedString;// Return the sanitized string
+                }
+                else
+                {
+                    Console.WriteLine("Something is wrong.");// Something is wrong
+                }
+            }
+            else
+            {
+                Console.WriteLine("Title not found.");// Title not found
+            }
+            return "";// Return an empty string if the title is not found
+        }
+
+        // Method to sanitize a string by removing special characters
+        static string SanitizeString(string input)
+        {
+            // Replace Turkish characters with their English equivalents
+            string result = input
+                .Replace("ç", "c")
+                .Replace("Ç", "C")
+                .Replace("ğ", "g")
+                .Replace("Ğ", "G")
+                .Replace("ı", "i")
+                .Replace("İ", "I")
+                .Replace("ö", "o")
+                .Replace("Ö", "O")
+                .Replace("ş", "s")
+                .Replace("Ş", "S")
+                .Replace("ü", "u")
+                .Replace("Ü", "U");
+
+            // Replace other characters with "-"
+            result = new string(result
+                .ToCharArray()
+                .Select(c => char.IsLetterOrDigit(c) || c == '-' ? c : '-')
+                .ToArray());
+
+            // Replace multiple "-" signs with a single "-"
+            result = Regex.Replace(result, @"-+", "-");
+
+            return result;// Return the sanitized string
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            RenameFiles();// Call the RenameFiles method
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/sametcn99/MarkdownFileRenamer");
+        }
+    }
+}
